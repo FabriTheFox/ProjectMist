0d7514d3b69f94c9b30c2bf8717f1f94768dd906
FabriTheFox <fabrithefox@gmail.com>
2019-03-27 19:09:10 +0100

    Eventing

--- ProjectMist/projects/engine/code/EventSystem/EventSystem.h
+++ ProjectMist/projects/engine/code/EventSystem/EventSystem.h
@@ -18,49 +18,75 @@ namespace ME
 
     RTTI_IMPLEMENTATION(Event);
 
-    class IEventDispatcher
-    {
-    public:
-        virtual void Dispatch(Event* theevent) = 0;
-    };
-
-    template<typename Ev>
-    class EventDispatcher : public IEventDispatcher
-    {
-    public:
-        typedef std::function<void(const Ev&)> ListenerFunc;
-        EventDispatcher(ListenerFunc func) : m_func(func) {}
-
-        void Dispatch(Event* theevent) override final
-        {
-            m_func(*((Ev*)theevent));
-        }
-
-        ListenerFunc m_func;
-    };
-
     class EventSystem
     {
         RTTI_DECLARATION(EventSystem);
         SINGLETON_DEFCTOR(EventSystem);
 
     public:
-        template <typename Ev>
-        void RegisterListener(std::function<void(const Ev&)> lisfunc)
+        template <typename Ev, typename Obj>
+        void RegisterListener(void (Obj::*lisfunc)(const Ev&), Obj* instance)
         {
-            auto& disp = m_Dispatchers[Ev.GetRTTI()];
-            disp.push_back(std::make_unique<EventDispatcher>(lisfunc));
+            auto& disp = m_Dispatchers[Ev::sGetRTTI()];
+            disp.push_back(std::make_unique<EventDispatcher<Ev, Obj>>(lisfunc, instance));
+        }
+
+        template <typename Ev>
+        void UnRegisterListener(void* lisfunc, void* instance = nullptr)
+        {
+            auto& itr = m_Dispatchers.find(Ev::sGetRTTI());
+            if (itr == m_Dispatchers.end())
+                return;
+            for (auto& d : itr->second)
+            {
+                if (d->IsSameAs(lisfunc, instance))
+                {
+                    d = itr->second.back();
+                    itr->second.pop_back();
+                    return;
+                }
+            }
         }
 
         template <typename Ev>
         void DispatchEvent(const Ev& theevent)
         {
-            auto itr = m_Dispatchers.find(Ev.GetRTTI());
+            auto itr = m_Dispatchers.find(Ev::sGetRTTI());
             if (itr == m_Dispatchers.end())
                 return;
             for (auto& d : itr->second)
-                d->Dispatch(&theevent);
+                d->Dispatch((Event*)&theevent);
         }
+
+    private:
+        class IEventDispatcher
+        {
+        public:
+            virtual void Dispatch(Event* theevent) = 0;
+            virtual void* GetObjectInstance() const = 0;
+            virtual void* GetFunctionPtr() const = 0;
+            bool IsSameAs(void* funct, void* obj) { return GetObjectInstance() == obj && GetFunctionPtr() == funct; }
+        };
+
+        template<typename Ev, typename Obj>
+        class EventDispatcher : public IEventDispatcher
+        {
+        public:
+            typedef void (Obj::*ListenerFunc)(const Ev&);
+            EventDispatcher(void (Obj::*lisfunc)(const Ev&), Obj* instance) : m_Function(lisfunc) {}
+
+            void* GetObjectInstance() const override final { return m_Instance; }
+            virtual void* GetFunctionPtr() const override final { return m_Function; }
+
+            void Dispatch(Event* theevent) override final 
+            { 
+                (m_Instance->*m_Function)(*((Ev*)theevent));
+            }
+
+        private:
+            Obj* m_Instance;
+            ListenerFunc m_Function;
+        };
 
         using DispatcherArray = std::vector<Uptr<IEventDispatcher>>;
         std::map<RTTI, DispatcherArray> m_Dispatchers;
--- ProjectMist/projects/game/code/GameMain.cpp
+++ ProjectMist/projects/game/code/GameMain.cpp
@@ -8,19 +8,47 @@
 
 #include <vector>
 
+class ExplosionEvent : public ME::Event
+{
+    RTTI_DECLARATION(ExplosionEvent)
+public:
+    ME::String name;
+};
 
+RTTI_IMPLEMENTATION(ExplosionEvent)
+
+class ListeningClass
+{
+public:
+    void OnExplosionEvent(const ExplosionEvent& explo)
+    {
+        std::cout << explo.name << std::endl;
+    }
+
+    void OnExplosionEvent2(const ExplosionEvent& explo)
+    {
+        std::cout << explo.name << "(again)" << std::endl;
+    }
+};
 
 int main()
 {
-    
-
-
-
-
     auto& t = ME::Terminal::GetInstance();
     t.GetWindow().SetWindowName("Liya");
     auto& w = t.GetWindow();
     w.CreateTheWindow();
+
+    auto& evs = ME::EventSystem::GetInstance();
+    ListeningClass ExplosionListener;
+
+    evs.RegisterListener<ExplosionEvent, ListeningClass>(&ListeningClass::OnExplosionEvent, &ExplosionListener);
+    //evs.RegisterListener<ExplosionEvent>(std::bind(&ListeningClass::OnExplosionEvent2, ExplosionListener, std::placeholders::_1));
+
+    ExplosionEvent explosion;
+    explosion.name = "BOOM";
+
+    evs.DispatchEvent(explosion);
+    evs.DispatchEvent(explosion);
 
     while (true)
     {
